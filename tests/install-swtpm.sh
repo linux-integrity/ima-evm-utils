60e1535438b63b324ffa8d47e3ebc6859626f2fc
Petr Vorel <pvorel@suse.cz>
2020-08-13 20:25:28 +0200

    install-swtpm.sh: Update ibmtpm to version 1637
    Signed-off-by: Petr Vorel <pvorel@suse.cz>
    Reviewed-by: Bruno Meneguele <bmeneg@redhat.com>(Fedora,CentOS 8(RHEL actually))
    Signed-off-by: Mimi Zohar <zohar@linux.ibm.com>

--- tests/install-swtpm.sh
+++ tests/install-swtpm.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-
 set -ex
-wget --no-check-certificate https://sourceforge.net/projects/ibmswtpm2/files/ibmtpm1332.tar.gz/download
-mkdir ibmtpm1332
-cd ibmtpm1332
+
+version=1637
+
+wget --no-check-certificate https://sourceforge.net/projects/ibmswtpm2/files/ibmtpm${version}.tar.gz/download
+mkdir ibmtpm$version
+cd ibmtpm$version
 tar -xvzf ../download
 cd src
 make -j$(nproc)
