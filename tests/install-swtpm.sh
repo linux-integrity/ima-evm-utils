5b764057f3992a744fc29e0ec1efb40f64e350f0
Petr Vorel <pvorel@suse.cz>
2020-08-13 20:25:27 +0200

    install-swtpm.sh: Ignore certificate for download
    Some distros in Travis CI (e.g. Debian and Ubuntu) have problems with
    downloading from sourceforge.net due unknown certificate issuer:
    
    --2020-08-11 14:47:51--  https://sourceforge.net/projects/ibmswtpm2/files/ibmtpm1332.tar.gz/download
    Resolving sourceforge.net (sourceforge.net)... 216.105.38.13
    Connecting to sourceforge.net (sourceforge.net)|216.105.38.13|:443... connected.
    ERROR: The certificate of 'sourceforge.net' is not trusted.
    ERROR: The certificate of 'sourceforge.net' doesn't have a known issuer.
    
    This is a preparation for future commit (moving to docker based Travis CI).
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>
    Reviewed-by: Bruno Meneguele <bmeneg@redhat.com>(Fedora,CentOS 8(RHEL actually))
    Signed-off-by: Mimi Zohar <zohar@linux.ibm.com>

--- tests/install-swtpm.sh
+++ tests/install-swtpm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -ex
-wget https://sourceforge.net/projects/ibmswtpm2/files/ibmtpm1332.tar.gz/download
+wget --no-check-certificate https://sourceforge.net/projects/ibmswtpm2/files/ibmtpm1332.tar.gz/download
 mkdir ibmtpm1332
 cd ibmtpm1332
 tar -xvzf ../download
