8e0b3f00becaecddbc2d460c781d70e329c2a71c
Petr Vorel <pvorel@suse.cz>
2021-04-15 13:27:28 +0200

    tests/install-swtpm.sh: Add tar option --no-same-owner
    to workaround running out of subuids/subgids when using podman:
    tar: ./LICENSE: Cannot change ownership to uid 339315, gid 578953: Invalid argument
    
    (run script under sudo would also work, but this does not require it)
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Mimi Zohar <zohar@linux.ibm.com>

--- tests/install-swtpm.sh
+++ tests/install-swtpm.sh
@@ -6,7 +6,7 @@ version=1637
 wget --no-check-certificate https://sourceforge.net/projects/ibmswtpm2/files/ibmtpm${version}.tar.gz/download
 mkdir ibmtpm$version
 cd ibmtpm$version
-tar -xvzf ../download
+tar --no-same-owner -xvzf ../download
 cd src
 make -j$(nproc)
 sudo cp tpm_server /usr/local/bin/
