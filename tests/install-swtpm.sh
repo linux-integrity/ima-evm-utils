aef36466c97027b23942164aa3ec683c9e6d8802
Vitaly Chikunov <vt@altlinux.org>
2021-07-12 08:16:43 +0300

    CI: Do not use sudo when not needed
    Some distributions, such as ALT, cannot use sudo under root by default.
    Error message will appear:
    
      root is not in the sudoers file.  This incident will be reported.
    
    Signed-off-by: Vitaly Chikunov <vt@altlinux.org>
    Signed-off-by: Mimi Zohar <zohar@linux.ibm.com>

--- tests/install-swtpm.sh
+++ tests/install-swtpm.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
-set -ex
+#!/bin/sh -ex
+
+# No need to run via sudo if we already have permissions.
+# Also, some distros do not have sudo configured for root:
+#   `root is not in the sudoers file.  This incident will be reported.'
+if [ -w /usr/local/bin ]; then
+	SUDO=
+else
+	SUDO=sudo
+fi
 
 version=1637
 
@@ -9,5 +17,5 @@ cd ibmtpm$version
 tar --no-same-owner -xvzf ../download
 cd src
 make -j$(nproc)
-sudo cp tpm_server /usr/local/bin/
+$SUDO cp tpm_server /usr/local/bin/
 cd ../..
