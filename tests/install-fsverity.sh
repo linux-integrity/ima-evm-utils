c8126e0f9f2073e0722ee135de7de8a5669cf5ef
Stefan Berger <stefanb@linux.ibm.com>
2023-12-01 08:31:29 -0500

    tests: Address issues raised by shellcheck SC2164
    Address issues raised by shellcheck SC2164:
      "Use cd ... || exit in case cd fails."
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    Signed-off-by: Mimi Zohar <zohar@linux.ibm.com>

--- tests/install-fsverity.sh
+++ tests/install-fsverity.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 git clone https://git.kernel.org/pub/scm/fs/fsverity/fsverity-utils.git
-cd fsverity-utils
+cd fsverity-utils || exit 1
 CC=gcc make -j"$(nproc)"
 cd ..
--- tests/install-mount-idmapped.sh
+++ tests/install-mount-idmapped.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 git clone https://github.com/brauner/mount-idmapped.git
-cd mount-idmapped
+cd mount-idmapped || exit 1
 gcc -o mount-idmapped mount-idmapped.c
 cd ..
