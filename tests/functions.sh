0e7c3097bb56476de6b356176e184a34382ca999
Stefan Berger <stefanb@linux.ibm.com>
2023-12-01 08:31:28 -0500

    tests: Address issues raised by shellcheck SC2034
    Address issues raised by shellcheck SC2034:
      "foo appears unused. Verify it or export it."
    
    Export PKCS11_KEYURI in a separate statement to avoid the following
    shellcheck issue:
    
      SC2155 (warning): Declare and assign separately to avoid masking
                        return values.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    Signed-off-by: Mimi Zohar <zohar@linux.ibm.com>

--- tests/functions.sh
+++ tests/functions.sh
@@ -371,6 +371,7 @@ _softhsm_setup() {
   if msg=$(./softhsm_setup setup 2>&1); then
     echo "softhsm_setup setup succeeded: $msg"
     PKCS11_KEYURI=$(echo "$msg" | sed -n 's|^keyuri: \(.*\)|\1|p')
+    export PKCS11_KEYURI
 
     export EVMCTL_ENGINE="--engine pkcs11"
     export OPENSSL_ENGINE="-engine pkcs11"
