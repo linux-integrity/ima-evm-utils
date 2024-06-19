6adc50f68202294e7c603b7fc60edfd018c33542
Dmitry Kasatkin <dmitry.kasatkin@gmail.com>
2024-06-19 17:42:41 +0300

    Change license to LGPL-2.0-or-later and GPL-2.0-or-later
    Currently libimaevm provided by this project is used by the tool evmctl,
    which is also provided by this project.
    
    An issue was reported about using libimaevm with other software. Its
    GPL2-only license makes it incompatible to use with other licenses, in
    particular GPL3-only.
    
    To address this issue, change the project license to GPL-2.0-or-later
    and libimaevm to LGPL 2.0 or later.
    
    This patch includes all acks.
    
    Link: https://lore.kernel.org/linux-integrity/9205ed32da8814d8c4d9bde5a03d92c7588f1d9a.1708503094.git.dmitry.kasatkin@gmail.com
    Link: https://lore.kernel.org/linux-integrity/103252ffff09c607e83c887cab2e0af5404d62ff.1710774200.git.dmitry.kasatkin@gmail.com
    Signed-off-by: Dmitry Kasatkin <dmitry.kasatkin@gmail.com>
    Acked-by: Dmitry Kasatkin <dmitry.kasatkin@huawei.com>
    Acked-by: Petr Vorel <pvorel@suse.cz>
    Acked-by: Mimi Zohar <zohar@linux.ibm.com> # all IBM contributions
    Acked-by: Stefan Berger <stefanb@linux.ibm.com>
    Acked-by: Vitaly Chikunov <vt@altlinux.org>
    Acked-by: Ken Goldman <kgold@linux.ibm.com>
    Acked-by: James Bottomley <James.Bottomley@HansenPartnership.com>
    Acked-by: Bruno Meneguele <bmeneg@redhat.com>
    Acked-by: Roberto Sassu <roberto.sassu@huawei.com>
    Acked-by: George Wilson <gcwilson@linux.ibm.com>
    Acked-by: Tergel Myanganbayar <tergelmyanganbayar2024@u.northwestern.edu>
    Acked-by: André Draszik <git@andred.net>
    Acked-by: Frank Sorenson <sorenson@redhat.com>
    Acked-by: Stephen Smalley <stephen.smalley.work@gmail.com>
    Acked-by: Vivek Goyal <vgoyal@redhat.com>
    Acked-by: Alberto Mardegan <a.mardegan@omp.ru>
    Acked-by: Eric Biggers <ebiggers@google.com> # all Google contributions
    Acked-by: Dmitry Baryshkov <dbaryshkov@gmail.com>
    Acked-by: Patrick Uiterwijk <patrick@puiterwijk.org>
    Acked-by: Kai Svahn <kai.svahn@linux.intel.com> # all Intel contributions
    Acked-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Acked-by: yh00.jung@samsung.com # all Samsung contributions
    Acked-by: dmitry.kasatkin@gmail.com # all Samsung contributions with consent from Samsung corporation

--- src/pcr.h
+++ src/pcr.h
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
 int tpm2_pcr_supported(void);
 int tpm2_pcr_read(const char *algo_name, uint32_t pcr_handle, uint8_t *hwpcr,
 		 int len, char **errmsg);
--- src/pcr_ibmtss.c
+++ src/pcr_ibmtss.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Support PCR reading implementation based on IBM TSS2
  *
