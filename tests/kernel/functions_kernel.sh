18a43267d33acf6428a599817e0a4c31c555b891
Roberto Sassu <roberto.sassu@huawei.com>
2025-03-19 14:28:36 +0100

    Add get_private_key() and load_public_key() to functions_kernel.sh
    Move get_private_key() and load_public_key() into the new library,
    functions_kernel.sh.
    
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>

--- tests/kernel/functions_kernel.sh
+++ tests/kernel/functions_kernel.sh
@@ -82,3 +82,89 @@ check_load_ima_rule() {
 
 	return "$OK"
 }
+
+get_private_key() {
+	local key_path
+	local key_path_copy="$1"
+
+	if [ -n "$TST_KEY_PATH" ]; then
+		if [ "${TST_KEY_PATH:0:1}" != "/" ]; then
+			echo "${RED}Absolute path required for the kernel signing key${NORM}"
+			return "$FAIL"
+		fi
+
+		if [ ! -f "$TST_KEY_PATH" ]; then
+			echo "${RED}Kernel signing key not found in $TST_KEY_PATH${NORM}"
+			return "$FAIL"
+		fi
+
+		key_path="$TST_KEY_PATH"
+	elif [ -f "$PWD/../../signing_key.pem" ]; then
+		key_path="$PWD/../../signing_key.pem"
+	elif [ -f "$PWD/../../signing_key.der" ]; then
+		key_path="$PWD/../../signing_key.der"
+	elif [ -f "/lib/modules/$(uname -r)/source/certs/signing_key.pem" ]; then
+		key_path="/lib/modules/$(uname -r)/source/certs/signing_key.pem"
+	elif [ -f "/lib/modules/$(uname -r)/build/certs/signing_key.pem" ]; then
+		key_path="/lib/modules/$(uname -r)/build/certs/signing_key.pem"
+	else
+		echo "${CYAN}Kernel signing key not found${NORM}"
+		return "$SKIP"
+	fi
+
+	if openssl pkey -inform der -in "$key_path" -text &> /dev/null; then
+		openssl pkey -in "$key_path" -out "$key_path_copy" -outform pem
+	else
+		cat "$key_path" > "$key_path_copy"
+	fi
+
+	result=$?
+	if [ $result -ne 0 ]; then
+		echo "${RED}Failed to convert/copy the kernel signing key${NORM}"
+		return "$FAIL"
+	fi
+
+	return "$OK"
+}
+
+load_public_key() {
+	local key_path="$1"
+	local keyring="$2"
+	local cert_path cert_path_der
+
+	if [ -n "$TST_CERT_PATH" ]; then
+		if [ "${TST_CERT_PATH:0:1}" != "/" ]; then
+			echo "${RED}Absolute path required for the kernel certificate${NORM}"
+			return "$FAIL"
+		fi
+
+		if [ ! -f "$TST_CERT_PATH" ]; then
+			echo "${RED}Kernel certificate not found in $TST_CERT_PATH${NORM}"
+			return "$FAIL"
+		fi
+
+		cert_path="$TST_CERT_PATH"
+	else
+		cert_path="$key_path"
+	fi
+
+	# We must always filter the cert, since the kernel rejects key+cert in DER form.
+	if ! cert_path_der=$(mktemp); then
+		echo "${RED}Failed to create temporary file for the kernel certificate${NORM}"
+		return "$FAIL"
+	fi
+
+	if ! openssl x509 -in "$cert_path" -out "$cert_path_der" -outform der; then
+		echo "${RED}Failed to convert the kernel certificate${NORM}"
+		return "$FAIL"
+	fi
+
+	if ! keyctl padd asymmetric pubkey %keyring:"$keyring" < "$cert_path_der" &> /dev/null; then
+		echo "${RED}Kernel public key cannot be added to the $keyring keyring${NORM}"
+		rm -f "$cert_path_der"
+		return "$FAIL"
+	fi
+
+	rm -f "$cert_path_der"
+	return "$OK"
+}
