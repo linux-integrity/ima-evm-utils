547fa247cce0388b7cfc8637c5fc86d5c747c7b2
Stefan Berger <stefanb@linux.ibm.com>
2026-05-04 11:00:36 -0500

    examples: Allow passing key algorithm to ima-genkey.sh
    Allow passing a key algorithm parameter to ima-genkey.sh and support
    a few more algorithms.
    
    Also support -? and --help to display the help screen.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- examples/ima-genkey.sh
+++ examples/ima-genkey.sh
@@ -6,5 +6,29 @@ DIR=$(dirname "$0")
 cd "${DIR}" 1>/dev/null || exit 1
 
 . ./functions
-ima_gen_signing_key rsa:2048
+
+#default key algorithm
+keyalgo=rsa:2048
+
+if [ "$1" = "-?" ] || [ "$1" = "--help" ]; then
+	cat <<_EOF_
+Create an EVM/IMA file signing key with a given algorithm.
+
+Usage: $0 [options] keyalgo
+
+The following key algorithms are supported:
+  ${SUPPORTED_ALGORITHMS}
+
+The following options are supported:
+    -?, --help  : Display this help screen and exit
+
+_EOF_
+	exit 0
+fi
+
+if [ "$1" != "" ]; then
+	keyalgo="$1"
+fi
+
+ima_gen_signing_key "${keyalgo}"
 exit $?
